53f7d6a0dd977e1dcc701f377c80eecf57ea89df
Pietroxero <115331791+Pietroxero@users.noreply.github.com>
2022-12-05 19:09:25 -0800

    added in html for scoring portion and functions to clear scores and reset the game

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -148,32 +148,7 @@ var quiz = [
   //start quiz after clicking start
   beginBtn.onclick = runQuiz;
   
-  //this will be for the highscores btn
-  var scoreBtn = document.querySelector('#viewscores');
-  
-  //this will be the rankings of players who have used this.
-  function showScores (){
-      var scores = window.localStorage.getItem(scores) || [];
-      scores.sort(function (a,b) {
-          return b.scores-a.scores;
-      });
-      scores.forEach(function(scores){
-          var listItems = document.createElement("li");
-          listItems.textContent = scores.yourname + "-" +scores.scores;
-          var orderList = document.getElementById("scores");
-          orderList.appendChild(listItems);
-      });
-  }
-  
-  //this clears previous scoring
-  function clearScores() {
-      window.localStorage.remove("scores");
-      window.location.reload();
-  
-  }
 
-  //document.getElementById("clear").onClick = clearScores;  // doesn't appear to be an element with and id of clear in the index.html
-  showScores();
   
   
   
